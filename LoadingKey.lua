a426d77c9f91a1f24a187363e97da55d32d7fa10
WeshkyB <therealsxirbes@gmail.com>
2025-05-15 01:22:05 +0200

    Update LoadingKey.lua

--- LoadingKey.lua
+++ LoadingKey.lua
@@ -1,3 +1,3 @@
 -- Yes its here now,
 -- so i can change it some times ive got nothing to do and dont need to obfuscate again and again...
-return "ASUISGAYANDHESMOMHASACOOK-JJFLW0OFFIZOIVDBH"
+return "key"
