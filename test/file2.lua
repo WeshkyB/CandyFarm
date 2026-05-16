6a5c7d94b7f2b80aa561ed921d2617ce15aa5d84
WeshkyB <therealsxirbes@gmail.com>
2026-05-16 20:54:45 +0200

    Update file2.lua
    Encapsulated tab creation in a function that takes a window parameter.

--- test/file2.lua
+++ test/file2.lua
@@ -1,5 +1,9 @@
-local mainTab = window.new({ text = "Auto Farm" })
-local databaseTab = window.new({ text = "Database" })
-local specialTab = window.new({ text = "Special" })
-local clientTab = window.new({ text = "Client" })
-local creditsTab = window.new({ text = "Credits" })
+return function(window)
+
+    local mainTab = window.new({ text = "Auto Farm" })
+    local databaseTab = window.new({ text = "Database" })
+    local specialTab = window.new({ text = "Special" })
+    local clientTab = window.new({ text = "Client" })
+    local creditsTab = window.new({ text = "Credits" })
+
+end
