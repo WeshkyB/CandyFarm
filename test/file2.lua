2935a9e33a2169fcd1ae7c067d02fc33acc13f30
WeshkyB <therealsxirbes@gmail.com>
2026-05-16 20:42:32 +0200

    Update file2.lua

--- test/file2.lua
+++ test/file2.lua
@@ -1,1 +1,5 @@
-
+local mainTab = window.new({ text = "Auto Farm" })
+local databaseTab = window.new({ text = "Database" })
+local specialTab = window.new({ text = "Special" })
+local clientTab = window.new({ text = "Client" })
+local creditsTab = window.new({ text = "Credits" })
